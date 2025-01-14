610af8fbafb01be97fc97494d790dec150fa20ff
2dam <2dam@TartangaLH.eus>
2025-01-14 08:54:51 +0100

    RESTClient de Ticket

---
+++ NocturnaClient/src/logic/TicketManagerFactory.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import rest.TicketRESTClient;
+
+
+
+/**
+ *
+ * @author 2dam
+ */
+public class TicketManagerFactory {
+      private static TicketManager ticketManager;
+    
+    public static EventManager get(){
+        if(ticketManager == null){
+            ticketManager = new TicketRESTClient();
+        }
+        return (EventManager) new TicketRESTClient();
+    }
+}
